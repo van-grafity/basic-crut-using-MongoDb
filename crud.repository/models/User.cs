ea5315b52519de2e7e3c0cca6f9c092314c2a22b
agung jati <agungjati94@gmail.com>
2019-04-21 20:45:26 +0700

    Add project files.

---
+++ crud.repository/models/User.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace crud.repository.models
+{
+    [BsonIgnoreExtraElements]
+    public class User
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public DateTime Created { get; set; }
+    }
+}
---
+++ crud.repository/services/UserService.cs
@@ -0,0 +1,51 @@
+﻿using crud.repository.models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace crud.repository.services
+{
+    
+
+    public class UserService
+    {
+        private readonly IMongoCollection<User> _users;
+
+        public UserService(string config)
+        {
+            var client = new MongoClient(config);
+            var database = client.GetDatabase("crud");
+            _users = database.GetCollection<User>("Users");
+        }
+        public async Task<User> Insert(User user)
+        {
+            user.Created = DateTime.Now;
+            await _users.InsertOneAsync(user).ConfigureAwait(false);
+            return user;
+        }
+
+        public async Task<User> Find(string email)
+        {
+            var user =  await _users.FindAsync<User>(u => u.Email == email).ConfigureAwait(false);
+            return await user.FirstOrDefaultAsync().ConfigureAwait(false);
+        }
+
+        public async Task Update(User user)
+        {
+            //var filter = Builders<User>.Filter.Eq(u => u.Id, user.Id);
+            var update = Builders<User>.Update
+                                       .Set(x => x.FirstName, user.FirstName)
+                                       .Set(x => x.LastName, user.LastName)
+                                       .Set(x => x.Email, user.Email);
+
+            await _users.UpdateOneAsync<User>( u => u.Id == user.Id, update).ConfigureAwait(false);
+        }
+
+        public async Task Delete(Guid Id)
+        {
+            await _users.DeleteOneAsync(u => u.Id == Id).ConfigureAwait(false);
+        }
+    }
+}
